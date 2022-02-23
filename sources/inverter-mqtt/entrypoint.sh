61ae805aba19502b9f3f55ba518000bdb1c4a9d9
Wayne ross <mr_trekkie@yahoo.com>
2022-02-22 20:22:05 -0500

    Get an actual 30s scan rate

--- sources/inverter-mqtt/entrypoint.sh
+++ sources/inverter-mqtt/entrypoint.sh
@@ -8,5 +8,23 @@ export TERM=xterm
 # Run the MQTT Subscriber process in the background (so that way we can change the configuration on the inverter from home assistant)
 /opt/inverter-mqtt/mqtt-subscriber.sh &
 
-# execute exactly every 30 seconds...
-watch -n 30 /opt/inverter-mqtt/mqtt-push.sh > /dev/null 2>&1
+sleepUntilNextSecs() { # args sec_quantity
+    # figure the next multiple of sec_quantity and sleep until then
+    local slp now
+    printf -v now '%(%s)T' -1
+    slp=$(( $now%$1 ))
+    if (( slp > 0 )); then
+      slp=$(( $1-$slp ))
+    fi
+    # printf 'sleep %ss, -> %(%c)T\n' $slp $((now+slp))
+    sleep $slp
+}
+
+# wait until the clock lines up on the next integer multiple seconds
+sleepUntilNextSecs 30
+# after which we'll execute exactly every 30 seconds...
+while true
+do
+  /opt/inverter-mqtt/mqtt-push.sh > /dev/null 2>&1
+  sleepUntilNextSecs 30
+done
